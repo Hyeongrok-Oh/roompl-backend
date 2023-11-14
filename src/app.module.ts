54bfba8c12fcaed5f0bd48ca80bd6edd216b80f4
Hyeong Rok Oh <hyeongrokoh@Hyeongui-MacBookAir.local>
2023-11-15 01:14:32 +0900

    added config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,29 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { BusinessModule } from './business/business.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "hyeongrokoh",
+      password: "12345",
+      database: "roompl",
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
-    }), BusinessModule],
+    }),
+    BusinessModule],
   controllers: [],
   providers: [],
 })
