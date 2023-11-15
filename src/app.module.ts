edf5a4877b10d1efc02bf6e289255a90b766cee1
Hyeong Rok Oh <hyeongrokoh@Hyeongui-MacBookAir.local>
2023-11-15 12:17:59 +0900

    added validation to config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import * as Joi from "joi";
 import { GraphQLModule } from '@nestjs/graphql';
 import { BusinessModule } from './business/business.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -9,14 +10,23 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev','prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      })
     }),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "hyeongrokoh",
-      password: "12345",
-      database: "roompl",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
     }),
